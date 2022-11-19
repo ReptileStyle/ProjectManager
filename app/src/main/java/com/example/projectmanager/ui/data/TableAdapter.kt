ddb739fad791ee8f1a30d870e1337b289c35a82b
ReptileStyle <kuzevanovii@inbox.ru>
2022-11-19 19:10:51 +0700

    some data fragment changes

--- app/src/main/java/com/example/projectmanager/ui/data/TableAdapter.kt
+++ app/src/main/java/com/example/projectmanager/ui/data/TableAdapter.kt
@@ -4,11 +4,14 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TableRow
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.projectmanager.R
-import com.example.projectmanager.ui.renameme.Work
+import com.example.projectmanager.ui.renameme.*
+import com.google.android.material.card.MaterialCardView
 import java.util.LinkedList
+
 
 class TableAdapter(
     private val workList: List<Work>
@@ -25,16 +28,27 @@ class TableAdapter(
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item=workList[position]
+//        holder.materialCardView.apply {
+//            radius=8f
+//            strokeWidth=2
+//        }
+        holder.row.apply {
+            if (position%2==1) setBackgroundColor(context.getColor(R.color.green))
+            else setBackgroundColor(context.getColor(R.color.green_light))
+        }
         holder.columnName.text=item.name
         holder.columnDuration.text=item.duration.toString()
-        holder.columnRequiredWorks.text=item.requiredWorks.toString()
+        holder.columnRequiredWorks.text=item.requiredWorks.toStr()
     }
+
 
     override fun getItemCount(): Int {
         return workList.count()
     }
 
     class ItemViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+       // val materialCardView: MaterialCardView = itemView.findViewById(R.id.data_card_view)
+        val row: TableRow=itemView.findViewById(R.id.data_row)
         val columnName: TextView =itemView.findViewById(R.id.data_column_1_value)
         val columnDuration: TextView =itemView.findViewById(R.id.data_column_2_value)
         val columnRequiredWorks: TextView =itemView.findViewById(R.id.data_column_3_value)
--- app/src/main/java/com/example/projectmanager/ui/renameme/Work.kt
+++ app/src/main/java/com/example/projectmanager/ui/renameme/Work.kt
@@ -1,5 +1,11 @@
 package com.example.projectmanager.ui.renameme
 
+
+
 class Work(val name:String,val duration:Int,val requiredWorks:List<Work>) {
 
+}
+
+fun List<Work>.toStr():String{
+    return this.joinToString(", ") { it.name }
 }
